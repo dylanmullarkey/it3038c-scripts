b71445a8205e8960c69732ea17f3a726abe8b32c
Dylan Mullarkey <mullardt@mail.uc.edu>
2023-09-24 16:23:36 -0400

    Week 3 Lab

---
+++ pws-scripts/sysinfodetailed.ps1
@@ -0,0 +1,13 @@
+﻿function getIP{
+    (get-netipaddress).ipv4address | select-string "192.*"
+}
+
+$IP = getIP
+$User = $env:USERNAME
+$ver = $HOST.Version
+$HOSTNAME = hostname
+$DATE = Get-Date
+
+$BODY = "This machine's IP is $IP. User is $User. Hostname is $HOSTNAME. PowerShell $ver. Today's date is $DATE."
+
+$BODY > c:\sysinfodetail.txt
