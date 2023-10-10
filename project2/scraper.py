9372d519e444ad92ceb9fd506e0e18e696d3d065
Dylan Mullarkey <DylanMullarkey10@gmail.com>
2023-10-09 20:46:39 -0400

    Add comments

--- project2/scraper.py
+++ project2/scraper.py
@@ -3,10 +3,12 @@
 # pip install requests
 # best to do this in a virtual python environment so it doesn't mess with future projects.
 
+
+#youtube uses JavaScript so I will need to revisit this as I don't have the time to research and complete. 
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.microcenter.com/search/search_results.aspx?Ntt=GeForce+RTX+4090&Ntx=mode+MatchPartial&Ntk=all&sortby=match&N=4294966998&myStore=true'
+url = 'https://www.youtube.com/@archmdz/videos'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
@@ -16,23 +18,7 @@ headers = {
 }
 response = requests.get(url, headers=headers )
 soup = BeautifulSoup(response.content, 'html.parser')
-products = soup.find_all('a', class_='productClickItemV2')
+uploads = soup.find_all('a', class_='productClickItemV2')
 
-print(products)
+print(uploads)
 
-"""  
-MAC_classes = html_soup.final_all('span', class_="event evowv_tb_event")
-filename = 'poolSchdule.csv'
-f = open(filename, 'w')
-
-csvHeaders = 'Event, Time \n'
-
-f.write(csvHeaders)
-
-for events in MAC_classes:
-    event = events.find('span', class_="event evowv_tb_event").text
-    time = events.find('span', class_="time").text
-
-    f.write(event + ',' + time)
-f.close()
-"""
