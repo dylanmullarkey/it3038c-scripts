8aced31e018e55aecbf3ee76714ba79624d76adb
Dylan Mullarkey <dylanmullarkeyf@gmail.com>
2023-11-30 15:50:58 -0500

    Progress at work

--- project3/files/index.js
+++ project3/files/index.js
@@ -1,86 +1,116 @@
 // grabbing start/reset button
-const startButton = document.getElementById("btn-start")
+const startButton = document.getElementById("btn-start");
 
 // grabbing bottle size buttons
-const btn16Oz = document.getElementById("btn-16oz")
-const btn20Oz = document.getElementById("btn-20oz")
-const btn24Oz = document.getElementById("btn-24oz")
-const btn32Oz = document.getElementById("btn-32oz")
-const btn40Oz = document.getElementById("btn-40oz")
+const btn16Oz = document.getElementById("btn-16oz");
+const btn20Oz = document.getElementById("btn-20oz");
+const btn24Oz = document.getElementById("btn-24oz");
+const btn32Oz = document.getElementById("btn-32oz");
+const btn40Oz = document.getElementById("btn-40oz");
 
 // used when removing button highlight...
-const sizeButtons = [btn16Oz, btn20Oz, btn24Oz, btn32Oz, btn40Oz]
+const sizeButtons = [btn16Oz, btn20Oz, btn24Oz, btn32Oz, btn40Oz];
 // grabbing bottle count buttons
-const btnAdd = document.getElementById("btnAdd")
-const btnSubtract = document.getElementById("btnSub")
+const btnAdd = document.getElementById("btnAdd");
+const btnSubtract = document.getElementById("btnSub");
 
 // grabbing input fields for goal & timer
-const goalInput = document.getElementById("input-goal")
-const hoursInput = document.getElementById("input-hours")
-const minutesInput = document.getElementById("input-minutes")
+const goalInput = document.getElementById("input-goal");
+const hoursInput = document.getElementById("input-hours");
+const minutesInput = document.getElementById("input-minutes");
 
 // grabbing p element which shows progress
-const progressEl = document.getElementById("progress")
+const progressEl = document.getElementById("progress");
 
 // initializing variables for calculations
-let bottleSize = 24
-let hours = 0
-let minutes = 0
-let goalOz = 0
-let bottlesFinished = 0
-let selectedButton = null
+let bottleSize = 24;
+let hours = 0;
+let minutes = 0;
+let goalOz = 0;
+let bottlesFinished = 0;
+let validInput = false;
+
+let selectedButton = btn24Oz;
+highlightButton(btn24Oz);
 
 startButton.addEventListener("click", function () {
-  checkInput(goalInput, hoursInput, minutesInput)
-})
+  if (startButton.innerText === "Reset") {
+    location.reload();
+  } else {
+    checkInput(goalInput, hoursInput, minutesInput);
+    renderStart();
+  }
+});
 
 btn16Oz.addEventListener("click", function () {
-  setBottleSize(16)
-  highlightButton(btn16Oz)
-})
+  setBottleSize(16);
+  highlightButton(btn16Oz);
+});
 btn20Oz.addEventListener("click", function () {
-  setBottleSize(20)
-  highlightButton(btn20Oz)
-})
+  setBottleSize(20);
+  highlightButton(btn20Oz);
+});
 btn24Oz.addEventListener("click", function () {
-  setBottleSize(24)
-  highlightButton(btn24Oz)
-})
+  setBottleSize(24);
+  highlightButton(btn24Oz);
+});
 btn32Oz.addEventListener("click", function () {
-  setBottleSize(32)
-  highlightButton(btn32Oz)
-})
+  setBottleSize(32);
+  highlightButton(btn32Oz);
+});
 btn40Oz.addEventListener("click", function () {
-  setBottleSize(40)
-  highlightButton(btn40Oz)
-})
+  setBottleSize(40);
+  highlightButton(btn40Oz);
+});
 
 btnAdd.addEventListener("click", function () {
-  bottlesFinished++
-})
+  if (validInput) {
+    bottlesFinished++;
+  }
+});
 btnSubtract.addEventListener("click", function () {
-  bottlesFinished--
-})
+  if (validInput) {
+    bottlesFinished--;
+  }
+});
 
 // assigns bottleSize depending on button clicked
 function setBottleSize(size) {
-  bottleSize = size
+  bottleSize = size;
 }
 
+function updateProgress() {
+  let runningTotal = bottleSize * bottlesFinished;
+  return runningTotal;
+}
+
+function renderStart() {
+  if (validInput) {
+    let userGoalOz = goalInput.value;
+    progressEl.textContent = `0 oz. / ${userGoalOz} oz.`;
+    startButton.textContent = "Reset";
+    goalInput.classList.add("disabled-input");
+    hoursInput.classList.add("disabled-input");
+    minutesInput.classList.add("disabled-input");
+  } else {
+    console.log("Invalid input -- cannot start");
+    return;
+  }
+}
 // changes button background to show user selection
 function highlightButton(buttonId) {
-  buttonId.classList.add("selected")
-  removeBottleSizeHighlight(buttonId)
-  selectedButton = buttonId
+  buttonId.classList.add("selected");
+  removeBottleSizeHighlight(buttonId);
+  selectedButton = buttonId;
 }
 // removes highlight when new button is selected
 function removeBottleSizeHighlight(buttonId) {
   if (buttonId === selectedButton) {
-    return
+    return;
   } else {
     for (i = 0; i < sizeButtons.length; i++) {
       if (sizeButtons[i] === selectedButton) {
-        sizeButtons[i].classList.remove("selected")
+        sizeButtons[i].classList.remove("selected");
       }
     }
   }
@@ -89,9 +119,17 @@ function removeBottleSizeHighlight(buttonId) {
 function checkInput(input1, input2, input3) {
   for (i = 0; i < arguments.length; i++) {
     if (!arguments[i].value) {
-      arguments[i].setAttribute("style", `background-color: #e63946;`)
+      arguments[i].setAttribute("style", `background-color: #e63946;`);
+      validInput = false;
     } else {
-      arguments[i].setAttribute("style", `background-color: white;`)
+      arguments[i].setAttribute("style", `background-color: white;`);
+      validInput = true;
     }
+  }
+  if (input2 === false && input3 === true) {
+    input2.setAttribute("style", `background-color: white;`);
+  }
+  if (input3 === false && input2 === true) {
+    input3.setAttribute("style", `background-color: white;`);
   }
 }
