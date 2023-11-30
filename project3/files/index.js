1f86940077d10b35de54e792501512a05b06e96f
Dylan Mullarkey <DylanMullarkey10@gmail.com>
2023-11-29 23:21:08 -0500

    Working button highlight

--- project3/files/index.js
+++ project3/files/index.js
@@ -8,6 +8,8 @@ const btn24Oz = document.getElementById("btn-24oz")
 const btn32Oz = document.getElementById("btn-32oz")
 const btn40Oz = document.getElementById("btn-40oz")
 
+// used when removing button highlight...
+const sizeButtons = [btn16Oz, btn20Oz, btn24Oz, btn32Oz, btn40Oz]
 // grabbing bottle count buttons
 const btnAdd = document.getElementById("btnAdd")
 const btnSubtract = document.getElementById("btnSub")
@@ -26,24 +28,31 @@ let hours = 0
 let minutes = 0
 let goalOz = 0
 let bottlesFinished = 0
+let selectedButton = null
 
 startButton.addEventListener("click", function () {
   checkInput(goalInput, hoursInput, minutesInput)
 })
+
 btn16Oz.addEventListener("click", function () {
   setBottleSize(16)
+  highlightButton(btn16Oz)
 })
 btn20Oz.addEventListener("click", function () {
   setBottleSize(20)
+  highlightButton(btn20Oz)
 })
 btn24Oz.addEventListener("click", function () {
   setBottleSize(24)
+  highlightButton(btn24Oz)
 })
 btn32Oz.addEventListener("click", function () {
   setBottleSize(32)
+  highlightButton(btn32Oz)
 })
 btn40Oz.addEventListener("click", function () {
   setBottleSize(40)
+  highlightButton(btn40Oz)
 })
 
 btnAdd.addEventListener("click", function () {
@@ -56,15 +65,26 @@ btnSubtract.addEventListener("click", function () {
 function setBottleSize(size) {
   bottleSize = size
 }
-function removeHighlight() {
-  for (i = 0; i < arguments.length; i++) {
-    arguments[i].setAttribute("style", `background-color: white`)
-  }
-}
+
 // changes button background to show user selection
 function highlightButton(buttonId) {
-  arguments.setAttribute("style", `background-color: #dad7cd`)
+  buttonId.classList.add("selected")
+  removeBottleSizeHighlight(buttonId)
+  selectedButton = buttonId
 }
+// removes highlight when new button is selected
+function removeBottleSizeHighlight(buttonId) {
+  if (buttonId === selectedButton) {
+    return
+  } else {
+    for (i = 0; i < sizeButtons.length; i++) {
+      if (sizeButtons[i] === selectedButton) {
+        sizeButtons[i].classList.remove("selected")
+      }
+    }
+  }
+}
+
 function checkInput(input1, input2, input3) {
   for (i = 0; i < arguments.length; i++) {
     if (!arguments[i].value) {
